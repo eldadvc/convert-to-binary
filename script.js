980078179190eea3c13852696ec623e7207fc019
eldadvc <67348560+eldadvc@users.noreply.github.com>
2022-02-27 14:47:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,499 @@
+'use strict';
+
+let is128on = false;
+let is64on = false;
+let is32on = false;
+let is16on = false;
+let is8on = false;
+let is4on = false;
+let is2on = false;
+let is1on = false;
+const btn_128 = document.querySelector('.btn_128');
+const btn_64 = document.querySelector('.btn_64');
+const btn_32 = document.querySelector('.btn_32');
+const btn_16 = document.querySelector('.btn_16');
+const btn_8 = document.querySelector('.btn_8');
+const btn_4 = document.querySelector('.btn_4');
+const btn_2 = document.querySelector('.btn_2');
+const btn_1 = document.querySelector('.btn_1');
+const buttons = document.querySelector('.button');
+
+
+// let toggle128 = false;
+// let toggle64 = false;
+// let toggle32 = false;
+// let toggle16 = false;
+// let toggle8 = false;
+// let toggle4 = false;
+// let toggle2 = false;
+// let toggle1 = false;
+const btnRoll = document.querySelector('.roll-btn');
+const theNumberToConvert = document.querySelector('.number');
+// const btn128 = document.querySelector('.btn--128');
+// const bulb128 = document.querySelector('.bulb--128');
+// const btn64 = document.querySelector('.btn--64');
+// const bulb64 = document.querySelector('.bulb--64');
+// const btn32 = document.querySelector('.btn--32');
+// const bulb32 = document.querySelector('.bulb--32');
+// const btn16 = document.querySelector('.btn--16');
+// const bulb16 = document.querySelector('.bulb--16');
+// const btn8 = document.querySelector('.btn--8');
+// const bulb8 = document.querySelector('.bulb--8');
+// const btn4 = document.querySelector('.btn--4');
+// const bulb4 = document.querySelector('.bulb--4');
+// const btn2 = document.querySelector('.btn--2');
+// const bulb2 = document.querySelector('.bulb--2');
+// const btn1 = document.querySelector('.btn--1');
+// const bulb1 = document.querySelector('.bulb--1');
+// const allBulbs = document.querySelector('.bulb');
+
+let rnd;
+let answer = 0;
+
+// btnControl(false);
+// enable disable buttons using boolian (true buttons work)
+function btnControl(state) {
+    if (state === true) {
+        btn_128.disabled = false;
+        btn_64.disabled = false;
+        btn_32.disabled = false;
+        btn_16.disabled = false;
+        btn_8.disabled = false;
+        btn_4.disabled = false;
+        btn_2.disabled = false;
+        btn_1.disabled = false;
+    }
+    if (state === false) {
+        btn_128.disabled = true;
+        btn_64.disabled = true;
+        btn_32.disabled = true;
+        btn_16.disabled = true;
+        btn_8.disabled = true;
+        btn_4.disabled = true;
+        btn_2.disabled = true;
+        btn_1.disabled = true;
+    }
+}
+
+function isRight(num) {
+    return num === rnd;
+}
+
+function win() {
+    document.querySelector('.number-container').classList.add('number-container-win');
+    document.querySelector('.number').classList.add('number-win');
+    btnControl(false);
+}
+
+
+// console.log(bulb128);
+// console.log(bulb64);
+// console.log(theNumberToConvert);
+
+
+btn_128.addEventListener('click', function () {
+    if (is128on === false) {
+        btn_128.classList.add('bulbOn');
+        btn_128.setAttribute('value', '1');
+        is128on = true;
+        answer += 128;
+        console.log(answer);
+
+    } else if (is128on === true) {
+        console.log('click');
+        btn_128.classList.remove('bulbOn');
+        btn_128.setAttribute('value', '0');
+        is128on = false;
+        answer -= 128;
+        console.log(answer);
+    }
+    if (isRight(answer)) {
+        win();
+    }
+})
+btn_64.addEventListener('click', function () {
+    if (is64on === false) {
+        btn_64.classList.add('bulbOn');
+        btn_64.setAttribute('value', '1');
+        is64on = true;
+        answer += 64;
+        console.log(answer);
+
+    } else if (is64on === true) {
+        console.log('click');
+        btn_64.classList.remove('bulbOn');
+        btn_64.setAttribute('value', '0');
+        is64on = false;
+        answer -= 64;
+        console.log(answer);
+    }
+    if (isRight(answer)) {
+        win();
+    }
+})
+
+btn_32.addEventListener('click', function () {
+    if (is32on === false) {
+        btn_32.classList.add('bulbOn');
+        btn_32.setAttribute('value', '1');
+        is32on = true;
+        answer += 32;
+        console.log(answer);
+
+    } else if (is32on === true) {
+        console.log('click');
+        btn_32.classList.remove('bulbOn');
+        btn_32.setAttribute('value', '0');
+        is32on = false;
+        answer -= 32;
+        console.log(answer);
+    }
+    if (isRight(answer)) {
+        win();
+    }
+})
+
+btn_16.addEventListener('click', function () {
+    if (is16on === false) {
+        btn_16.classList.add('bulbOn');
+        btn_16.setAttribute('value', '1');
+        is16on = true;
+        answer += 16;
+        console.log(answer);
+
+    } else if (is16on === true) {
+        console.log('click');
+        btn_16.classList.remove('bulbOn');
+        btn_16.setAttribute('value', '0');
+        is16on = false;
+        answer -= 16;
+        console.log(answer);
+    }
+    if (isRight(answer)) {
+        win();
+    }
+})
+
+btn_8.addEventListener('click', function () {
+    if (is8on === false) {
+        btn_8.classList.add('bulbOn');
+        btn_8.setAttribute('value', '1');
+        is8on = true;
+        answer += 8;
+        console.log(answer);
+
+    } else if (is8on === true) {
+        console.log('click');
+        btn_8.classList.remove('bulbOn');
+        btn_8.setAttribute('value', '0');
+        is8on = false;
+        answer -= 8;
+        console.log(answer);
+    }
+    if (isRight(answer)) {
+        win();
+    }
+})
+
+btn_4.addEventListener('click', function () {
+    if (is4on === false) {
+        btn_4.classList.add('bulbOn');
+        btn_4.setAttribute('value', '1');
+        is4on = true;
+        answer += 4;
+        console.log(answer);
+
+    } else if (is4on === true) {
+        console.log('click');
+        btn_4.classList.remove('bulbOn');
+        btn_4.setAttribute('value', '0');
+        is4on = false;
+        answer -= 4;
+        console.log(answer);
+    }
+    if (isRight(answer)) {
+        win();
+    }
+})
+
+btn_2.addEventListener('click', function () {
+    if (is2on === false) {
+        btn_2.classList.add('bulbOn');
+        btn_2.setAttribute('value', '1');
+        is2on = true;
+        answer += 2;
+        console.log(answer);
+
+    } else if (is2on === true) {
+        console.log('click');
+        btn_2.classList.remove('bulbOn');
+        btn_2.setAttribute('value', '0');
+        is2on = false;
+        answer -= 2;
+        console.log(answer);
+    }
+    if (isRight(answer)) {
+        win();
+    }
+})
+
+btn_1.addEventListener('click', function () {
+    if (is1on === false) {
+        btn_1.classList.add('bulbOn');
+        btn_1.setAttribute('value', '1');
+        is1on = true;
+        answer += 1;
+        console.log(answer);
+
+    } else if (is1on === true) {
+        console.log('click');
+        btn_1.classList.remove('bulbOn');
+        btn_1.setAttribute('value', '0');
+        is1on = false;
+        answer -= 1;
+        console.log(answer);
+    }
+    if (isRight(answer)) {
+        win();
+    }
+})
+
+
+btnRoll.addEventListener('click', function () {
+    console.log('Click');
+    rnd = Math.trunc(Math.random() * 255) + 1;
+    console.log(`new number: ${rnd}`);
+    theNumberToConvert.textContent = rnd;
+    theNumberToConvert.classList.remove('hidden');
+    answer = 0;
+    is128on = false;
+    is64on = false;
+    is32on = false;
+    is16on = false;
+    is8on = false;
+    is4on = false;
+    is2on = false;
+    is1on = false;
+    btnControl(true);
+    // toggle128 = false;
+    // toggle64 = false;
+    // toggle32 = false;
+    // toggle16 = false;
+    // toggle8 = false;
+    // toggle4 = false;
+    // toggle2 = false;
+    // toggle1 = false;
+    // allBulbs.classList.remove('bulb-on');
+    // bulb128.classList.remove('bulb-on');
+    // bulb64.classList.remove('bulb-on');
+    // bulb32.classList.remove('bulb-on');
+    // bulb16.classList.remove('bulb-on');
+    // bulb8.classList.remove('bulb-on');
+    // bulb4.classList.remove('bulb-on');
+    // bulb2.classList.remove('bulb-on');
+    // bulb1.classList.remove('bulb-on');
+
+    // buttons.setAttribute('value', '0');
+    btn_128.setAttribute('value', '0');
+    btn_64.setAttribute('value', '0');
+    btn_32.setAttribute('value', '0');
+    btn_16.setAttribute('value', '0');
+    btn_8.setAttribute('value', '0');
+    btn_4.setAttribute('value', '0');
+    btn_2.setAttribute('value', '0');
+    btn_1.setAttribute('value', '0');
+
+    btn_128.classList.remove('bulbOn');
+    btn_64.classList.remove('bulbOn');
+    btn_32.classList.remove('bulbOn');
+    btn_16.classList.remove('bulbOn');
+    btn_8.classList.remove('bulbOn');
+    btn_4.classList.remove('bulbOn');
+    btn_2.classList.remove('bulbOn');
+    btn_1.classList.remove('bulbOn');
+
+
+    document.querySelector('.button').classList.remove('bulbOn');
+    document.querySelector('.number-container').classList.remove('number-container-win');
+    document.querySelector('.number').classList.remove('number-win');
+
+
+})
+
+btnControl(false);
+
+// btn128.addEventListener('click', function () {
+
+//     console.log('btn 128 click');
+//     if (toggle128 === false) {
+//         bulb128.classList.add('bulb-on');
+//         toggle128 = true;
+//         answer += 128;
+//         console.log(answer);
+//     } else if (toggle128 === true) {
+//         bulb128.classList.remove('bulb-on');
+//         toggle128 = false;
+//         answer -= 128;
+//         console.log(answer);
+//     }
+// if (isRight(answer)) {
+//     win();
+// }
+
+
+
+// })
+
+// btn64.addEventListener('click', function () {
+
+//     console.log('btn 64 click');
+//     if (toggle64 === false) {
+//         bulb64.classList.add('bulb-on');
+//         toggle64 = true;
+//         answer += 64;
+//         console.log(answer);
+//     } else if (toggle64 === true) {
+//         bulb64.classList.remove('bulb-on');
+//         toggle64 = false;
+//         answer -= 64;
+//         console.log(answer);
+//     }
+//     if (isRight(answer)) {
+//         win();
+//     }
+// })
+
+// btn32.addEventListener('click', function () {
+
+//     console.log('btn 32 click');
+//     if (toggle32 === false) {
+//         bulb32.classList.add('bulb-on');
+//         toggle32 = true;
+//         answer += 32;
+//         console.log(answer);
+//     } else if (toggle32 === true) {
+//         bulb32.classList.remove('bulb-on');
+//         toggle32 = false;
+//         answer -= 32;
+//         console.log(answer);
+//     }
+//     if (isRight(answer)) {
+//         win();
+//     }
+// })
+
+// btn16.addEventListener('click', function () {
+
+//     console.log('btn 16 click');
+//     if (toggle16 === false) {
+//         bulb16.classList.add('bulb-on');
+//         toggle16 = true;
+//         answer += 16;
+//         console.log(answer);
+//     } else if (toggle16 === true) {
+//         bulb16.classList.remove('bulb-on');
+//         toggle16 = false;
+//         answer -= 16;
+//         console.log(answer);
+//     }
+//     if (isRight(answer)) {
+//         win();
+//     }
+// })
+
+// btn8.addEventListener('click', function () {
+
+//     console.log('btn 8 click');
+//     if (toggle8 === false) {
+//         bulb8.classList.add('bulb-on');
+//         toggle8 = true;
+//         answer += 8;
+//         console.log(answer);
+//     } else if (toggle8 === true) {
+//         bulb8.classList.remove('bulb-on');
+//         toggle8 = false;
+//         answer -= 8;
+//         console.log(answer);
+//     }
+//     if (isRight(answer)) {
+//         win();
+//     }
+// })
+
+// btn4.addEventListener('click', function () {
+
+//     console.log('btn 4 click');
+//     if (toggle4 === false) {
+//         bulb4.classList.add('bulb-on');
+//         toggle4 = true;
+//         answer += 4;
+//         console.log(answer);
+//     } else if (toggle4 === true) {
+//         bulb4.classList.remove('bulb-on');
+//         toggle4 = false;
+//         answer -= 4;
+//         console.log(answer);
+//     }
+//     if (isRight(answer)) {
+//         win();
+//     }
+// })
+
+// btn2.addEventListener('click', function () {
+
+//     console.log('btn 2 click');
+//     if (toggle2 === false) {
+//         bulb2.classList.add('bulb-on');
+//         toggle2 = true;
+//         answer += 2;
+//         console.log(answer);
+//     } else if (toggle2 === true) {
+//         bulb2.classList.remove('bulb-on');
+//         toggle2 = false;
+//         answer -= 2;
+//         console.log(answer);
+//     }
+//     if (isRight(answer)) {
+//         win();
+//     }
+// })
+
+// btn1.addEventListener('click', function () {
+
+//     console.log('btn 1 click');
+//     if (toggle1 === false) {
+//         bulb1.classList.add('bulb-on');
+//         toggle1 = true;
+//         answer += 1;
+//         console.log(answer);
+//     } else if (toggle1 === true) {
+//         bulb1.classList.remove('bulb-on');
+//         toggle1 = false;
+//         answer -= 1;
+//         console.log(answer);
+//     }
+//     if (isRight(answer)) {
+//         win();
+//     }
+// })
+
+console.log(document.querySelector('.title'));
+
+
+// let isOn = false;
+
+// const tstBtn = document.querySelector('.tstBtn');
+// console.log(tstBtn);
+
+// tstBtn.addEventListener('click', function () {
+//     if (isOn === false) {
+//         tstBtn.classList.add('bulbOn');
+//         isOn = true;
+//     } else if (isOn === true) {
+//         console.log('click');
+//         tstBtn.classList.remove('bulbOn');
+//         isOn = false;
+//     }
+// })
+
+
